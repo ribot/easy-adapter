a81bea9e29e14d65fb9e3c741ac2c9bee1256c84
Ivan Carballo <ivan@ribot.co.uk>
2014-02-16 19:56:08 +0000

    Add onSetListeners() example to demo app and comment code properly

--- EasyAdapterDemo/src/main/java/uk/co/ribot/easyadapterdemo/MainActivity.java
+++ EasyAdapterDemo/src/main/java/uk/co/ribot/easyadapterdemo/MainActivity.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2014 Ribot Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.co.ribot.easyadapterdemo;
 
 import android.app.Activity;
@@ -17,6 +32,11 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         mListView = (ListView) findViewById(R.id.list_view);
 
+        /*
+          Simply create an EasyAdapter by passing a Context, your ItemViewHolder class and the list of items.
+          Alternatively, you can create an EasyAdapter only with a Context and an ItemViewHolder class and set
+          the list of items later.
+         */
         mListView.setAdapter(new EasyAdapter<Person>(this, PersonViewHolder.class, DataProvider.getListPeople()));
 
 
--- EasyAdapterDemo/src/main/java/uk/co/ribot/easyadapterdemo/PersonViewHolder.java
+++ EasyAdapterDemo/src/main/java/uk/co/ribot/easyadapterdemo/PersonViewHolder.java
@@ -1,8 +1,24 @@
+/*
+ * Copyright (C) 2014 Ribot Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.co.ribot.easyadapterdemo;
 
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import uk.co.ribot.easyadapter.ItemViewHolder;
 import uk.co.ribot.easyadapter.PositionInfo;
@@ -11,11 +27,16 @@ import uk.co.ribot.easyadapter.annotations.ViewId;
 
 /**
  * Created by ivan on 14/02/2014.
+ * <p/>
+ * The extension of ItemViewHolder for the ListView of Person.
  */
 
+//Annotate the class with the layout ID of the item.
 @LayoutId(R.layout.person_item_layout)
 public class PersonViewHolder extends ItemViewHolder<Person> {
 
+    //Annotate every field with the ID of the view in the layout.
+    //The views will automatically be assigned to the fields.
     @ViewId(R.id.image_view_person)
     ImageView imageViewPerson;
 
@@ -25,14 +46,27 @@ public class PersonViewHolder extends ItemViewHolder<Person> {
     @ViewId(R.id.text_view_phone)
     TextView textViewPhone;
 
+    //Extend ItemViewHolder and call super(view)
     public PersonViewHolder(View view) {
         super(view);
     }
 
+    //Override onSetValues() to set the values of the items in the views.
     @Override
     public void onSetValues(Person person, PositionInfo positionInfo) {
         imageViewPerson.setImageResource(person.getResDrawableId());
         textViewName.setText(person.getName());
         textViewPhone.setText(person.getPhoneNumber());
+    }
+
+    //Optionally override onSetListeners to add listeners to the views.
+    @Override
+    public void onSetListeners(final Person item, PositionInfo positionInfo) {
+        imageViewPerson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getView().getContext(), "My name is " + item.getName() + "!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
