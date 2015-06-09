3f271b68485409d2357db7ec42104ba44279e31a
Ivan Carballo <ivan@ribot.co.uk>
2015-06-09 11:22:16 +0100

    Add unit tests for EasyAdapter class

--- demo/src/test/java/uk/co/ribot/easyadapterdemo/util/DefaultConfig.java
+++ demo/src/test/java/uk/co/ribot/easyadapterdemo/util/DefaultConfig.java
@@ -3,4 +3,5 @@ package uk.co.ribot.easyadapterdemo.util;
 public class DefaultConfig {
     //The api level that Robolectric will use to run the unit tests
     public static final int EMULATE_SDK = 21;
+    public static final String PACKAGE_NAME = "uk.co.ribot.easyadapterdemo";
 }
